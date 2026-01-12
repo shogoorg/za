14510e07ad3d21798c2daef828ba87a367ee1bad
shogoorg <shogoorg@gmail.com>
2026-01-12 17:14:22 +0900

    Create za

---
+++ adk_agent/za/agent.py
@@ -0,0 +1,26 @@
+import os
+import dotenv
+from za import tools  # za に変更
+from google.adk.agents import LlmAgent
+
+dotenv.load_dotenv()
+
+PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT', 'project_not_set')
+
+maps_toolset = tools.get_maps_mcp_toolset()
+bigquery_toolset = tools.get_bigquery_mcp_toolset()
+
+root_agent = LlmAgent(
+    model='gemini-3-pro-preview',
+    name='root_agent',
+    instruction=f"""
+                Help the user answer questions by strategically combining insights from two sources:
+                
+                1.  **BigQuery toolset:** Access facility-level emissions, national rankings, and administrative data in the 'za' dataset. Do not use any other dataset.
+                Run all query jobs from project id: {PROJECT_ID}.
+
+                2.  **Maps Toolset:** Use this for geographic analysis of emission sources (e.g., factories, power plants) and visualizing their locations.
+                Include a hyperlink to an interactive map in your response where appropriate.
+            """,
+    tools=[maps_toolset, bigquery_toolset]
+)
